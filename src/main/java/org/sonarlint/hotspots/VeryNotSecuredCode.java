f9bff99c60a484fb137e19e5883274a8e477dc79
agigleux <gigleux_alexandre@yahoo.fr>
2018-06-20 14:57:36 +0200

    First commit

---
+++ src/main/java/org/sonarlint/hotspots/VeryNotSecuredCode.java
@@ -0,0 +1,24 @@
+package org.sonarlint.hotspots;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.servlet.http.Cookie;
+
+public class VeryNotSecuredCode {
+
+  public Cookie createCookie() {
+    Cookie cookie = new Cookie("myCookieName", "12");
+    return cookie;
+  }
+
+  public void cryptMe(String rawText) throws NoSuchAlgorithmException, NoSuchPaddingException {
+    Cipher c = Cipher.getInstance("DESede/ECB/PKCS5Padding");
+  }
+
+  public void useMD5orSha1() throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("SHA1");
+  }
+
+}
