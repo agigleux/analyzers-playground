2a86c65f9c0be89e700411d8263256a8eddb763d
Alexandre Gigleux <alexandre.gigleux@sonarsource.com>
2022-12-15 10:52:18 +0100

    Update S2139.java

--- src/main/java/S2139/S2139.java
+++ src/main/java/S2139/S2139.java
@@ -6,6 +6,9 @@ import java.sql.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+
 public class S2139 {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(S2139.class);
@@ -30,25 +33,17 @@ public class S2139 {
     }
   }
 
-  public void executeSQL2(Connection con, String sqlQuery) throws MySQLException {
-    Statement stmt = null;
-    try {
-      stmt = con.createStatement();
-      stmt.execute(sqlQuery);
-    } catch (SQLException e) {
-      LOGGER.error("SQLException", e);
-      throw new MySQLException(e);
-    } finally {
-      if (stmt != null) {
-        try {
-          stmt.close();
-        } catch (SQLException e) {
-          LOGGER.error("SQLException", e);
-          throw new MySQLException(e);
-        }
+  public void runUnsafeMatches(HttpServletRequest request) throws IOException {
+      String folder = request.getParameter("folder");
+
+      String cmd = "mkdir " + folder;
+
+      if (!cmd.matches("mkdir [a-zA-Z]++")) {
+          throw new IllegalArgumentException();
       }
-    }
-  }  
+
+      Runtime.getRuntime().exec(cmd);
+  }
   
   public class MySQLException extends Exception {
 
